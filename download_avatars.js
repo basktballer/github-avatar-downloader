45ae91a725d9fa02d898edd28a7613e608f9a690
Eden Yeung <basktballer@gmail.com>
2019-04-08 21:52:18 +0000

    Fixed authorization token issue. Added dotenv for token access. Added error handling.

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var fs = require('fs');
 var dotenv = require('dotenv').config();
+var not200flag = false;
 
 
 console.log("Welcome to the GitHub Avatar Downloader!");
@@ -10,12 +11,17 @@ function getRepoContributors(repoArr, cb) {
   var options = {
     url: 'https://api.github.com/repos/' + repoArr[0] + "/" + repoArr[1] + "/contributors",
     headers: {
-      'User-Agent': 'request'
-    },
-    authorization: process.env.GITHUB_TOKEN // authorization token saved in secret.js file
+      'User-Agent': 'request',
+      'Authorization': 'token ' + process.env.GITHUB_TOKEN // authorization token saved in secret.js file
+    }
   };
 
   request(options, function(err, res, body) { //run request with options object
+    console.log('Status Code:',res.statusCode);
+    if (res.statusCode !== 200) {
+      not200flag = true;
+    }
+    
     cb(err, body);  // call back called with body response
   }); 
 
@@ -42,14 +48,24 @@ getRepoContributors(getInput(), saveRepoContributors);
 function saveRepoContributors(err, result) {
   // function that checks user inputs are valid for searching and calls downloadImageByURL in a for loop to download all files
   var data = JSON.parse(result);
-  var fp = '';
+  var fp = 'avatars/';
+
+  if (!fs.existsSync(fp)) { // if avatars directory doesn't exist, create it
+    fs.mkdirSync(fp);
+    console.log("avatars file path created.")
+  }
 
   if (getInput().length !== 2) {
     console.log("Incorrect number of parameters passed. Please input exactly 2 values for repoOwner and repoName.")
+  } else if (data.message === "Not Found") {
+    console.log("Repo owner or repo name not found. Please check spelling and re-input.")
+  } else if (not200flag === true) {
+      console.log (data.message);
   } else {
+    
     for (var i = 0; i < data.length; i++) {
-      fp = "avatars/" + data[i].login;
-      downloadImageByURL(data[i].avatar_url, fp);
+      var path = fp + data[i].login;
+      downloadImageByURL(data[i].avatar_url, path);
     }
   }
 };
