35ff21df4c9b3e00b48043b4b04d7fe477cb532f
Eden Yeung <basktballer@gmail.com>
2019-04-08 18:50:17 +0000

    Download avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var secret = require('./secrets')
 
 console.log("Welcome to the GitHub Avatar Downloader!");
@@ -19,10 +20,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 }
 
+function downloadImageByURL (url, filepath) {
+  // ...
+  request.get(url)
+        .on('error', function(err) {
+          throw err;
+        })
+        .on('response', function(response) {
+          console.log("Downloading file.");          
+        })
+        .pipe(fs.createWriteStream(filepath))
+        .on('finish', function() {
+          console.log("Download complete!")
+        });
+}
+
 getRepoContributors("lighthouse-labs", "tweeter", function(err, result) {
   var data = JSON.parse(result);
+  var fp = '';
 
   for (var i = 0; i < data.length; i++) {
-    console.log("Avatar URL:", data[i].avatar_url);
+    fp = "avatars/" + data[i].login;
+    downloadImageByURL(data[i].avatar_url, fp);
   }
 });
